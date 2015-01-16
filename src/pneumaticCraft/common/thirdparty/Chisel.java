2b88762ef8de05dbdded73adc4f9b6f3a3c9ddf2
Clienthax <clienthax@gmail.com>
2015-01-16 12:44:52 +0000

    Add support for chisel 2
    Signed-off-by: Clienthax <clienthax@gmail.com>

--- src/pneumaticCraft/common/thirdparty/Chisel.java
+++ src/pneumaticCraft/common/thirdparty/Chisel.java
@@ -4,37 +4,58 @@ import pneumaticCraft.api.PneumaticRegistry;
 import pneumaticCraft.lib.Log;
 import cpw.mods.fml.relauncher.ReflectionHelper;
 
-public class Chisel implements IThirdParty{
+public class Chisel implements IThirdParty {
 
-    @Override
-    public void preInit(){
+	@Override
+	public void preInit() {
 
-    }
+	}
 
-    @Override
-    public void init(){
+	@Override
+	public void init() {
 
-    }
+	}
 
-    @Override
-    public void postInit(){
-        try {
-            Class modClass = Class.forName("info.jbcs.minecraft.chisel.Chisel");
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCTMId", "renderCTMId").getInt(null));
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderEldritchId", "renderEldritchId").getInt(null));
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCarpetId", "renderCarpetId").getInt(null));
-        } catch(Throwable e) {
-            Log.error("Chisel reflection failed:");
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void postInit() {
 
-    @Override
-    public void clientSide(){
+		Class modClass = null;
+		//Original chisel
+		try {
+			Log.info("Attempting Chisel v1 integration");
+			modClass = Class.forName("info.jbcs.minecraft.chisel.Chisel");
+		} catch (ClassNotFoundException e) {
+			Log.info("Chisel v1 class info.jbcs.minecraft.chisel.Chisel not found, not integrating");
+		}
 
-    }
+		//Chisel v2
+		try {
+			Log.info("Attempting Chisel v2 integration");
+			modClass = Class.forName("com.cricketcraft.chisel.Chisel");
+		} catch (ClassNotFoundException e) {
+			Log.info("Chisel v2 class com.cricketcraft.chisel.Chisel not found, not integrating");
+		}
 
-    @Override
-    public void clientInit(){}
+		if (modClass != null) {
+			try {
+				PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCTMId", "renderCTMId").getInt(null));
+				PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderEldritchId", "renderEldritchId").getInt(null));
+				PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCarpetId", "renderCarpetId").getInt(null));
+				Log.info("Chisel integrated");
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void clientSide() {
+
+	}
+
+	@Override
+	public void clientInit() {
+	}
 
 }
